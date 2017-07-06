370e9162198de818f31d039c2f2433d8184a8d1b
Bogdan <bogdan.vey2002@gmail.com>
2017-07-06 15:34:29 +0300

    Пошук шляху з найбільшою кількістю зерна

--- f.cpp
+++ f.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int n,m,a[100][100];
+	int n,m,a[101][101],b[200];
 	cin>>n>>m;
 	for(int i=0;i<100;i++){
 		for (int j=0;j<100;j++){
@@ -24,7 +24,33 @@ int main(){
 			}
 		}
 	}
-	cout<<a[n][m];
+	int k=n+m-3;
+	int i=n;
+	int j=m;
+	while(n!=1 || m!=1){
+		if(a[n][m-1]>a[n-1][m]){
+			b[k]=1;
+			m-=1;
+		}
+		else{
+			b[k]=0;
+			n-=1;
+		}
+		
+		k--;
+	}
+	
+	k=i+j-3;
+	for(int i=0;i<=k;i++){
+		if(b[i]==1){
+			cout<<'R';
+		}
+		else{
+			cout<<'F';
+		}
+	}
+	
 	cin>>n;
+	
 }
 
