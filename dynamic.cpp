331250a22ff20c60e05e01e3d04fc9c8e34fec0c
Bogdan <bogdan.vey2002@gmail.com>
2017-07-06 15:36:50 +0300

    Пошук максимальної кількості зерна

---
+++ dynamic.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n,m,a[101][101],b[1000];
+	cin>>n>>m;
+	for(int i=0;i<100;i++){
+		for (int j=0;j<100;j++){
+			a[i][j]=0;
+		}
+	}
+		for(int i=1;i<n+1;i++){
+		for (int j=1;j<m+1;j++){
+			cin>>a[i][j];
+		}
+
+	}
+	for(int i=1;i<n+1;i++){
+		for (int j=1;j<m+1;j++){
+			if (a[i-1][j]>a[i][j-1]){
+				a[i][j]+=a[i-1][j];
+			}
+			else{
+				a[i][j]+=a[i][j-1];
+			}
+		}
+	}
+}
