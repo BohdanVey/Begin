01588f9ac3bfb9eecb006a685e2fc223dc351877
Bogdan <bogdan.vey2002@gmail.com>
2017-07-28 18:23:19 +0300

    Update Dijkstra.cpp

--- Graphs/Dijkstra.cpp
+++ Graphs/Dijkstra.cpp
@@ -1,67 +1,74 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<math.h>
 using namespace std;
-int n,start,m,p,d,q;
-
-vector<int> D,visited,k;//prime-звідки,mark=чи вже проходили
-
-    vector<vector<int >> w;
-vector<int> Di(int st){
-    int mini=n+1;
-    k.resize(n);
-    k[st]=0;
-    visited[st]=true;
-    for(int i=0;i<n;i++){
-         D[i]=w[st][i];
-    }
-     int index=0,u=0;
-     for (int i=0;i<n;i++)
-    {
-        int mini=6000000;
-        for (int j=0;j<n;j++){
-            if(visited[j]==false){
-           
-         D[j]=min(D[j],D[i]+w[i][j]);
-    
-         if(D[j]<mini){
-             mini=D[j];
-             u=j;
-         }
-            }
-        }
-        visited[u]=true;
-        k[u]=D[u];
-        
+vector<vector<int>>weight;
+vector<bool> used;
+	vector<int> dist;
+	int f;
+int n;
+vector<int>Dijkstra(int start){
+	
+	
+	for(int i=0;i<n;i++){
+		dist[i]=weight[start][i];
+	}
+	for(int i=0;i<n;i++){
+used[i]=false;
+	}
+	used[start]=true;
+	int mini=1000001;
+	int ind=start;
+	int u=start;
+	for(int i=0;i<n;i++){
+		mini=1000000;
+			for(int j=0;j<n;j++){
+				if(used[j]==false){
+					
+					if(dist[j]>dist[ind]+weight[ind][j]){
+						dist[j]=dist[ind]+weight[ind][j];
+						
+						}
+					if(mini>dist[j]){
+							mini=dist[j];
+							 u=j;
+					}
+				}
+			}
+			ind=u;
+			used[u]=true;
+	}
+	return dist;
+}
+int main(){
+	int st,fi;
+	cin>>n>>st>>fi;
+	weight.resize(n);
+	used.resize(n);
+	dist.resize(n);
+	for(int i=0;i<n;i++){
+		weight[i].resize(n);
+	}
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			cin>>f;
+			weight[i][j]=f;
+			if(f==-1){
+				weight[i][j]=1000000;
+			}
+		}
+	}
+	
+	st--;
+	fi--;
+	dist=Dijkstra(st);
+	if(dist[fi]==1000000){
+		cout<<"-1";
+	}
+	else{
+	cout<<dist[fi];
+	}
+	cin>>f;
 }
 
-return k;
-}   
-int main()
-{
-cin>>n>>m>>start;
-D.resize(n);
-start--;
-visited.resize(n);
-w.resize(n);
-for(int i=0;i<n;i++){
-    visited[i]=false;
-    w[i].resize(n);
-    for(int j=0;j<n;j++){
-        w[i][j]=6000000;
-    }
-}
-for(int i=0;i<m;i++){
-   cin>>p>>d>>q;
-   p--;
-   d--;
-   w[p][d]=q;
-   w[d][p]=q;
-}
-
-k=Di(start);
-for(int i=0;i<n;i++){
-    cout<<k[i]<<" ";
-}
-}
