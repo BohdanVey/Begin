0fc5cd34698461ba678fecce4d38101060c26a06
Bogdan <bogdan.vey2002@gmail.com>
2017-07-30 14:04:51 +0300

    k

---
+++ Eiler.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<math.h>
+using namespace std;
+int booth(int n){
+	int res=n;
+	for(int i=2;i*i<=n;i++){
+		if(n%i==0)
+		while(n%i==0)
+			n/=i;
+			res-=res/i;
+		
+		}
+			if (n > 1)
+		res -= res / n;
+	return res;
+	
+
+	
+}
+
+int main(){
+int a,n;
+cin>>n;
+cout<<booth(n);
+cin>>a;
+}
